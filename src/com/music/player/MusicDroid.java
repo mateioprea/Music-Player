e337f5dacc393bbaa42c1fb0675399db36215f05
root <root@matei-Satellite-C660.(none)>
2012-09-21 18:58:56 +0300

    Simple music player. Work in progress

---
+++ src/com/music/player/MusicDroid.java
@@ -0,0 +1,23 @@
+package com.music.player;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+class Mp3Filter implements FilenameFilter {
+    public boolean accept(File dir, String name) {
+        return (name.endsWith(".mp3"));
+    }
+}
